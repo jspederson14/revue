f107332e2b7fb90f3b40802deacb4da59985297c
Joe Pederson <joepederson@Joes-MacBook-Air.local>
2021-04-06 17:51:19 -0500

    changed config

--- server/config.py
+++ server/config.py
@@ -1,7 +1,2 @@
-#reading in json file to data var
-import json
-f = open('keys.json',)
-data = json.load(f)
-
-flask_secret_key = data["flask_secret_key"]
+flask_secret_key = "TEMPKEY"
 image_upload_folder = ".../revue/server/images"
